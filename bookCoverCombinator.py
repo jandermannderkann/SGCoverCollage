bb94c8f699e8c452074b7ce78da38788ee4e08cb
Jan Koppenhagen <jan.koppenhagen@web.de>
2024-12-31 16:36:07 +0100

    Fix wrong image combinations

--- bookCoverCombinator.py
+++ bookCoverCombinator.py
@@ -4,7 +4,9 @@ import math
 # 210 x 297 mm
 def combineImages(files: list[str], path:str):
     images = [Image.open(x) for x in files ]
-    images = [x for x in images if x.size[0]>100]
+    images = [x for x in images if not x.size[0]<100] # Amazon images
+    images = [x for x in images if not x.size[1]==248] # Not available images
+
     noImages = len(images)
     widths, heights = zip(*(i.size for i in images))
     print("widths, heights:")
@@ -55,12 +57,18 @@ def combineImages(files: list[str], path:str):
     i = 0
     row = 0
     for row in range(noRows):
+        start_row_x = 0
+        image_target_width = math.ceil(twidth/noCols)
+
         print("Drawing row {} with index {} and {} columns".format(row,i,noCols))
         imgs_row = scaled_images[i:i+noCols]
         if len(imgs_row) == 0:
             print("WARN not enough images in row {}".format(row))
             continue
-        
+        if len(imgs_row) < noCols:
+            missing = noCols-len(imgs_row)
+            start_row_x = missing/2*image_target_width
+            pass
         row_widths = widths[i:i+noCols]
         row_heights = heights[i:i+noCols]
         # # print("dimensions for row:")
@@ -71,24 +79,31 @@ def combineImages(files: list[str], path:str):
         
         # print("Row mean: {},{}".format(row_mean_width, row_mean_height))
         # row_scale_factor = (twidth/sum(row_widths))/noCols
-        max_height_final_images = 0
-        for col,img in enumerate(imgs_row):
+        final_images = resize_prop_to_fit_x(imgs_row, image_target_width)
+        row_img_heights = [img.size[1] for img in final_images]
+        max_height_final_images = math.ceil(sum(row_img_heights)/len(row_img_heights))
+        
+        for col,img in enumerate(final_images):
             img_x = img.size[0]
             img_y = img.size[1]
-            target_width = math.ceil(twidth/noCols)
-            scale = target_width/img_x
-            new_dim = (math.floor(img_x*scale), math.floor(img_y*scale))
-            # print("Resizing Image again to: {},{}".format(new_dim[0],new_dim[1]))
-            # print("Total width: {}".format(noCols*new_width))
-            final_img = img.resize(new_dim)
-            max_height_final_images = max(max_height_final_images, final_img.size[1])
-            # if x+img_x > twidth:
-            #     y+=img_y
-            #     x=0
-            print("Pasting resized image with dim {},{} at {},{}".format(final_img.size[0],final_img.size[1],col*new_width,y))
-            new_im.paste(final_img,(math.floor(col*new_width),y))
+            print("Pasting resized image with dim {},{} at {},{}".format(img.size[0],img.size[1],col*image_target_width,y))
+            new_im.paste(img,(math.floor(start_row_x+col*image_target_width),y))
         i+=noCols
         y+=max_height_final_images
 
     print("Saving image")
     new_im.save('test.jpg')
+
+def resize_prop_to_fit_x(imgs: list[Image], twidth):
+    resized = []
+    for img in imgs:
+        img_x = img.size[0]
+        img_y = img.size[1]
+        scale = twidth/img_x
+        new_dim = (math.floor(img_x*scale), math.floor(img_y*scale))
+        # print("Resizing Image again to: {},{}".format(new_dim[0],new_dim[1]))
+        # print("Total width: {}".format(noCols*new_width))
+        new_img = img.resize(new_dim)
+        resized.append(new_img)
+    return resized
+    
