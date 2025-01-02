4c9b45b8f88a39a933d7d120eae8cadc98585a4c
Jan Koppenhagen <jan.koppenhagen@web.de>
2025-01-02 14:02:24 +0100

    Fix crucial error in isbn definition

--- isbn.py
+++ isbn.py
@@ -1,6 +1,6 @@
 
 def isIsbn(str:str):
-    return len(str)==13
+    return len(str)==13 or len(str)==10 # there are 2 isbn variants: isbn10 and isbn13
 
 def isASIN(str:str):
     return len(str)==10 
